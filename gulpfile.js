26cd46bb6ea4c51c4dd8151a7f8151be7254e566
sole <sole@mozilla.com>
2014-07-27 22:35:34 +0100

    build app and app.min separately

--- gulpfile.js
+++ gulpfile.js
@@ -11,17 +11,28 @@ gulp.task('lint', function() {
 		.pipe(jshint.reporter('default'));
 });
 
-gulp.task('scripts', function() {
-	return gulp.src('src/*.js')
+gulp.task('app', function() {
+	return gulp.src(['src/libs/*.js', 'src/*.js'])
 		.pipe(concat('app.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+// Build minified production version including code for analytics
+gulp.task('app-min', function() {
+	return gulp.src(['src/libs/*.js', 'src/production/ga.js', 'src/*.js'])
+		.pipe(concat('app.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/*.js', ['lint', 'scripts']);
+gulp.task('html', function() {
+	return gulp.src('src/index.html')
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['lint', 'scripts', 'watch']);
+gulp.task('watch', function() {
+	gulp.watch('src/*.js', ['lint', 'app', 'app-min']);
+	gulp.watch('src/index.html', ['html']);
+});
+
+gulp.task('default', ['lint', 'app', 'app-min', 'html', 'watch']);
