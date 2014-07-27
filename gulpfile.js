3f1e96d4d091f8be18c99d5eb790764585d915e6
sole <sole@mozilla.com>
2014-07-27 23:07:34 +0100

    replace build area in html with compressed file and also copy uncompressed file using separate libraries

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var htmlreplace = require('gulp-html-replace');
 
 gulp.task('lint', function() {
 	return gulp.src('src/*.js')
@@ -13,7 +14,6 @@ gulp.task('lint', function() {
 
 gulp.task('app', function() {
 	return gulp.src(['src/libs/*.js', 'src/*.js'])
-		.pipe(concat('app.js'))
 		.pipe(gulp.dest('dist'));
 });
 
@@ -27,12 +27,21 @@ gulp.task('app-min', function() {
 
 gulp.task('html', function() {
 	return gulp.src('src/index.html')
+		.pipe(rename('index_uncompressed.html'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('html-min', function() {
+	return gulp.src('src/index.html')
+		.pipe(htmlreplace({
+			'app': 'app.min.js'
+		}))
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
 	gulp.watch('src/*.js', ['lint', 'app', 'app-min']);
-	gulp.watch('src/index.html', ['html']);
+	gulp.watch('src/index.html', ['html', 'html-min']);
 });
 
-gulp.task('default', ['lint', 'app', 'app-min', 'html', 'watch']);
+gulp.task('default', ['lint', 'app', 'app-min', 'html', 'html-min', 'watch']);
